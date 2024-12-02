2ffaaa80b2e71f92f756fb62fa9ad58aad2f26ea
Lewis Westwood Flood <15088984+lewis-wf@users.noreply.github.com>
2024-12-02 09:29:01 +0000

    Part 2 - terrible solution

--- 2024/day_02/solution.py
+++ 2024/day_02/solution.py
@@ -1,33 +1,49 @@
 from aoc_tools import *
 
+def process_line(line) -> tuple[bool, int | None]:
+    increasing = False
+    for ni, num in enumerate(line):
+        if ni == 0:
+            continue
+        if ni == 1:
+            increasing = True if num > line[ni-1] else False
+        if 0 < abs(num - line[ni-1]) <= 3:
+            if increasing and num > line[ni-1]:
+                continue
+            elif not increasing and num < line[ni-1]:
+                continue
+            else:
+                return False
+        else:
+            return False
+    
+    return True
+
 def find(input) -> int:
     lines = [get_numbers(line) for line in input]
 
-    min = 1
-    max = 3
     total = 0
     for line in lines:
-        valid = True
-        increasing = False
-        for ni, num in enumerate(line):
-            if ni == 0:
-                continue
-            if ni == 1:
-                increasing = True if num > line[ni-1] else False
-                condition = () if increasing else (num < line[ni-1])
-            if 0 < abs(num - line[ni-1]) <= 3:
-                if increasing and num > line[ni-1]:
-                    continue
-                elif not increasing and num < line[ni-1]:
-                    continue
-                else:
-                    valid = False
-                    break
-            else:
-                valid = False
+        llen = len(line)
+        index = 0
+
+        cline = line.copy()
+        while True:
+            valid = process_line(line)
+            if index == llen:
                 break
+
+            if not valid:
+                line = cline.copy()
+                del line[index]
+                index += 1
+            else:
+                break
+
         if valid:
             total += 1
+        else:
+            pass
     return total
         
                 
@@ -41,11 +57,11 @@ def test():
 1 3 6 7 9"""
     test_input = [line.strip().replace("\n", "") for line in test_input.split("\n")]
 
-    assert find(test_input) == 2
+    assert find(test_input) == 4
 
 def real():
     input = get_input_file("2024/day_02/")
     print(find(input))
     
-# test()
+test()
 real()
