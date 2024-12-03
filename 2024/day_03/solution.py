b9036bc13a6738c550e5db89e18451807432f1b7
Lewis Westwood Flood <15088984+lewis-wf@users.noreply.github.com>
2024-12-03 08:11:10 +0000

    Part 2 - regex regex regex

--- 2024/day_03/solution.py
+++ 2024/day_03/solution.py
@@ -3,25 +3,31 @@ import re
 from ast import literal_eval 
 
 def find(input) -> int:
-    regex_pattern = r"mul(\(\d{1,3},\d{1,3}\))"
-
-    mults = []
+    all_matches_pattern = r"mul(\(\d{1,3},\d{1,3}\))|(do\(\))|(don't\(\))"
+    
+    instructions = []
     for line in input:
-        res = re.findall(regex_pattern, line)
-        mults.extend(res)
+        res = re.findall(all_matches_pattern, line)
+        instructions.extend(res)
     
     total = 0
-    for r in mults:
-        tup_r = literal_eval(r)
-        total += tup_r[0] * tup_r[1]
+    do = True
+    for instruct_tup in instructions:
+        if instruct_tup[2]:
+            do = False
+        if instruct_tup[1] and not do:
+            do = True
+        if instruct_tup[0] and do:
+            tup_r = literal_eval(instruct_tup[0])
+            total += tup_r[0] * tup_r[1]
 
     return total
     
 
 def test():
-    input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    input = ["xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"]
 
-    assert find(input) == 161
+    assert find(input) == 48
 
 def real():
     input = get_input_file("2024/day_03/")
