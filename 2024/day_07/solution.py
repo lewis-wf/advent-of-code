10e16e4dfe081dc965ffeabf348cecd5561b96cd
Lewis Westwood Flood <15088984+lewis-wf@users.noreply.github.com>
2024-12-07 19:05:36 +0000

    Part 2 - simple with recursion :)

--- 2024/day_07/solution.py
+++ 2024/day_07/solution.py
@@ -12,12 +12,13 @@ def recur_check(target, cur_total, index, nums) -> bool:
         return cur_total == target
     
     addition = recur_check(target, cur_total + nums[index], index + 1, nums)
+    concat = recur_check(target, int(str(cur_total) + str(nums[index])), index + 1, nums)
 
     if cur_total == 0:
         cur_total = 1
     
     multiplication = recur_check(target, cur_total * nums[index], index + 1, nums)
-    return addition or multiplication
+    return addition or multiplication or concat
 
 
 def find(input):
@@ -47,7 +48,7 @@ def test():
 
     input = normalise_test_input(input)
 
-    assert find(input) == 3749
+    assert find(input) == 11387
 
 def real():
     input = get_input_file("2024/day_07/")
