ff545da29e59738938a27e10003a5fd25b76e85a
Lewis Westwood Flood <15088984+lewis-wf@users.noreply.github.com>
2024-12-08 19:36:50 +0000

    Remove commentary

--- 2024/day_08/solution.py
+++ 2024/day_08/solution.py
@@ -2,23 +2,6 @@ from collections import defaultdict
 import itertools
 from aoc_tools import *
 
-# Rules:
-# Freqs are single digits or letters. Case matters - a != A
-# This is about finding 'antinodes' which are exactly in line
-# with two antennas of the same frequency, but only in the line where the antenna are
-# exactly twice as far away as the other
-
-# Me:
-# I think this is another fancy offset Q - find the offset between each unique pair
-# of paired antenna, find their offset, then apply that offset
-# on each side/direction (when in bounds). Add those locations to set, get the  length
-# of the set.
-# The first step is to get the position of each letter into dict.
-# Then find each pair
-# then offsets
-# then offsets + 1 step in each direction
-# then see if we match the test!
-
 def gen_nodes(start_pos: tuple[int, int], offset: tuple[int, int], bounds: tuple[int, int]) -> list[tuple]:
     nodes = []
     nxt_pos = start_pos
