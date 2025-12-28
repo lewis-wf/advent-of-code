e16c9fe31e1d2f187ff1493307ed6781d2e6e933
Lewis Westwood Flood <15088984+lewis-wf@users.noreply.github.com>
2025-12-29 00:38:24 +0100

    Day 1 Part 2

--- 2025/day_01/src/main.rs
+++ 2025/day_01/src/main.rs
@@ -1,11 +1,21 @@
-use std::fs;
+use std::{fs, str::FromStr};
 
 fn main() {
     let START: i32 = 50;
     let DIAL_SIZE: i32 = 100;
-    let TARGET: i32 = 0;
 
     let input = fs::read_to_string("./input.txt").unwrap();
+//     let input = String::from_str("L68
+// L30
+// R48
+// L5
+// R60
+// L55
+// L1
+// L99
+// R14
+// L82").unwrap();
+
     let lines: Vec<&str> = input.split("\n").collect();
 
     let mut current_p = START;
@@ -14,16 +24,33 @@ fn main() {
     // instruction = L|R<d=i32> where d is the distance to move in that direction. lower <- left, right -> higher
     for instruction in lines {
         let (direction, movement) = instruction.split_at(1);
-        let movement_i: i32 = movement.parse().expect(&format!("Invalid movement int: {}", movement));
+        let movement_i: i32 = movement
+            .parse()
+            .expect(&format!("Invalid movement int: {}", movement));
 
-        current_p = match direction {
-            "R" => (current_p+movement_i) % DIAL_SIZE,
-            "L" => (current_p-movement_i) % DIAL_SIZE,
-            &_ => panic!()
+        let rot_value = match direction {
+            "R" => current_p + movement_i,
+            "L" => current_p - movement_i,
+            &_ => panic!(),
         };
 
-        if current_p == 0 {target_count += 1}
+        if rot_value < 0 && current_p != 0 { target_count += 1; }
+
+        let processed_value = rot_value.abs(); 
+
+        let hits = (processed_value / DIAL_SIZE);
+        println!("{} {} {} {}", instruction, current_p, rot_value, hits);
+        target_count += hits;
+
+        if processed_value == 0 {
+            target_count += 1
+        }
+
+        // use mod to the new value
+        current_p = rot_value.rem_euclid(DIAL_SIZE);
+        
+        println!("{} {}", current_p, target_count);
     }
 
-    print!("{}", target_count)
+    println!("{}", target_count)
 }
