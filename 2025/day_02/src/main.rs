17aacde0aacdb0940696a9651c08f56373bd237c
Lewis Westwood Flood <15088984+lewis-wf@users.noreply.github.com>
2025-12-29 11:19:46 +0100

    Day 2 Part 2

--- 2025/day_02/src/main.rs
+++ 2025/day_02/src/main.rs
@@ -1,5 +1,13 @@
 use std::{collections::btree_map::Range, fs, str::FromStr};
 
+fn rem_first_and_last(value: &str) -> &str {
+    // Taken from: https://stackoverflow.com/questions/65976432/how-to-remove-first-and-last-character-of-a-string-in-rust
+    let mut chars = value.chars();
+    chars.next();
+    chars.next_back();
+    chars.as_str()
+}
+
 fn main() {
     let input = fs::read_to_string("./input.txt").unwrap();
     // let input = String::from_str("11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124").unwrap();
@@ -13,9 +21,7 @@ fn main() {
             panic!("Length of parts does not equal 2!")
         }
 
-        let left: u64 = parts[0]
-            .parse()
-            .expect("Invalid left part int");
+        let left: u64 = parts[0].parse().expect("Invalid left part int");
         let right: u64 = parts[1].parse().expect("Invalid right part int");
 
         (left, right)
@@ -27,12 +33,18 @@ fn main() {
     // this is very expensive, yippee
     let mut target_sum = 0;
     for range in ranges {
-        for num in range.0..(range.1+1) {
+        for num in range.0..(range.1 + 1) {
             let num_str = num.to_string();
-            let num_parts = num_str.split_at(num_str.len()/2);
-            
-            // Now do the parts comparison
-            if num_parts.0 == num_parts.1 {
+
+            // Using the double concat trick to find repeated substrings
+            let doubled_str = num_str.clone() + &num_str;
+            // First we get the string minus the first and last chars
+            let stripped_double_string = rem_first_and_last(&doubled_str);
+
+            // Then, we just need to see if we can find the original string in the nw string
+            let first_occurence: Vec<_> = stripped_double_string.match_indices(&num_str).collect();
+
+            if first_occurence.len() > 0 {
                 target_sum += num
             }
         }
